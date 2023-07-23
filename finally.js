db9483b43ec7194849c1e7958e854b08809b4003
aashishrana <aashishrana71135@gmail.com>
2023-07-23 11:09:09 -0700

    finally code execute promise reject or resolve

---
+++ finally.js
@@ -0,0 +1,25 @@
+function morePromise() {
+    return new Promise((resolve, reject) => {
+        // Asyncc code
+        setTimeout(function f() {
+            console.log("async function")
+            reject("rejected");
+        }, 5000);
+    });
+}
+console.log("start");
+let x = morePromise();
+console.log("a new promise");
+x.then(function f2(value) {
+    console.log("after completion of async code", value)
+})
+.catch(function f3(value) {
+    console.log("new code ", value);
+}) 
+.finally(function f4() {
+    console.log("finally executed");  // After complete code the finally execute in end
+})
+console.log("end");
+for(let i = 0; i < 3; i++) {
+    console.log("simple function ");
+}
