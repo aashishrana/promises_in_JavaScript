7f7040e06f1c6f4c7980d12e0d272e0a0597298c
aashishrana <aashishrana71135@gmail.com>
2023-07-23 10:42:44 -0700

    error solved of second question

--- promise2.js
+++ promise2.js
@@ -1,5 +1,5 @@
 function promiseNew() {
-    return new promiseNew((resolve , reject) => {
+    return new Promise((resolve , reject) => {
         setTimeout(function f() {
             console.log("Asynchronus code");
             resolve("asynchronus code execute completly");
@@ -7,15 +7,41 @@ function promiseNew() {
         
     });
 };
-
 console.log("Start");
 let x = promiseNew();
-
+console.log("Got a new promise");
 x.then(function f2() {
     console.log("after the completion of async  code ");
 });
-
 console.log("end");
 for(let i = 0; i < 5; i++) {
    console.log("simple loop");
+}
+
+
+
+
+
+
+
+
+function createPromise() {
+    return new Promise((resolve, reject) => {
+        // Asynchronus piece of code
+        setTimeout(function f() {
+            console.log("timer done");
+            resolve("done");
+        }, 3000);
+    });
+}
+console.log("start");
+let y = createPromise();
+console.log("got a new promise");
+y.then(function f() {
+    console.log("promise done");
+});
+console.log("end");
+for(let i = 0; i < 5; i++) {
+    // code
+    console.log("Synchronus function");
 }
